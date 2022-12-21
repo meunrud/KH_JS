8e9e4adf7514a289736166394ad062c6ec7fcd82
meunrud <113085963+meunrud@users.noreply.github.com>
2022-12-21 21:09:07 +0900

    ✅ 팀 프로젝트 완성

--- 과제/js/회원가입양식.js
+++ 과제/js/회원가입양식.js
@@ -6,10 +6,12 @@ $('#inputID').on('input', function () {
     console.log($(this).val());
 
     if (regEx.test($(this).val())) {
-        $('#idCheck').css('color', '#3880e8');
+        $('#idCheckimg').attr('src', '');
+        $('#idCheckimg').attr('src', 'img/파란체크.png');
+        $('#inputID').css('backgroundColor', 'white');
         idMessage.text('');
     } else if (!regEx.test($(this).val())) {
-        $('#idCheck').css('color', '#dc3340');
+        $('#idCheckimg').attr('src', 'img/검정체크.png');
         idMessage.text(
             '회원 아이디(ID)는 띄어쓰기 없이 6~10자리의 영문자와 숫자 조합만 가능합니다.'
         );
@@ -35,10 +37,11 @@ $('#inputPW').on('input', function () {
     console.log($(this).val());
 
     if (regEx.test($(this).val())) {
-        $('#pwCheck').css('color', '#3880e8');
+        $('#pwCheckimg').attr('src', '');
+        $('#pwCheckimg').attr('src', 'img/파랑자물쇠.png');
         pwMessage.text('');
     } else if (!regEx.test($(this).val())) {
-        $('#pwCheck').css('color', '#dc3340');
+        $('#pwCheckimg').attr('src', 'img/검정자물쇠.png');
         pwMessage.text(
             '띄어쓰기 없는 8~15자의 영문 대/소문자, 숫자 또는 특수문자 조합으로 입력하셔야 합니다.'
         );
@@ -64,12 +67,13 @@ $('#pwConfirm').on('input', function () {
     console.log(pw == $(this).val());
     if (pw === $(this).val()) {
         pwConfirmMessage.text('');
-        $('#pwConfirmcheck').css('color', '#3880e8');
+        $('#pwConfrimCheckimg').attr('src', '');
+        $('#pwConfrimCheckimg').attr('src', 'img/파랑자물쇠.png');
     } else {
         pwConfirmMessage.text('');
         pwConfirmMessage.css('color', '#dc3340');
         pwConfirmMessage.text('비밀번호가 일치하지 않습니다.');
-        $('#pwConfirmcheck').css('color', '#dc3340');
+        $('#pwConfrimCheckimg').attr('src', 'img/검정자물쇠.png');
     }
 });
 $('#pwConfirm').blur(function () {
@@ -87,10 +91,12 @@ $('#inputName').on('input', function () {
     console.log($(this).val());
 
     if (regEx.test($(this).val())) {
-        $('#nameCheck').css('color', '#3880e8');
+        $('#nameCheckimg').attr('src', '');
+        $('#nameCheckimg').attr('src', 'img/파란체크.png');
         nameMessage.text('');
     } else if (!regEx.test($(this).val())) {
         $('#nameCheck').css('color', '#dc3340');
+        $('#nameCheckimg').attr('src', 'img/검정체크.png');
         nameMessage.text('한글로 입력해 주세요');
     }
 });
@@ -105,49 +111,125 @@ $('#inputName').blur(function () {
 });
 
 //*성별------------------------------------------------
+let genderMessage = $("span[name = 'genderErrorMessage']");
 $("input[name ='gender']").change(function () {
     const gender = $("input[name ='gender']");
     if (gender[0].checked || gender[1].checked) {
-        $('#genderCheck').css('color', '#3880e8');
+        $('#genderCheckimg').attr('src', '');
+        $('#genderCheckimg').attr('src', 'img/파란체크.png');
+    } else {
+        if (!gender[0].checked && !gender[1].checked) {
+            genderMessage.test('필수 정보입니다.');
+            $('#genderCheckimg').attr('src', 'img/검정체크.png');
+            return false;
+        }
     }
 });
 
 //*전화번호------------------------------------------------
-const middle = $('#middleOfPh').val();
-const end = $('#endOfPh').val();
-const message = $("span[name = 'phErrorMessage']");
+let middle = $('#middleOfPh');
+let end = $('#endOfPh');
+let phMessage = $("span[name = 'phErrorMessage']");
 
 $('.inputPh').on('click', function () {
     $('#divisionLine').css('color', 'black');
 });
 $('.inputPh').blur(function () {
     $('#divisionLine').css('color', 'white');
-    if (middle.length == 0 || end.length == 0) {
-        message.css('color', '#dc3340');
-        message.text('전화번호를 정확히 입력 해 주세요');
+    if (middle.val().length == 0 || end.val().length == 0) {
+        phMessage.css('color', '#dc3340');
+        phMessage.text('전화번호를 정확히 입력 해 주세요');
+    } else if (middle.val().length > 0 || end.val().length > 0) {
+        $('#divisionLine').css('color', 'black');
     }
 });
 
 $('.inputPh').on('input', function () {
-    const regEx = /^[0-9]{3,4}$/;
-    if (regEx.test(middle) && regEx.test(end)) {
-        $('#phCheck').css('color', '#3880e8');
-        message.text('');
+    const regEx1 = /^[0-9]{3,4}$/;
+    const regEx2 = /^[0-9]{4}$/;
+
+    if (regEx1.test(middle.val()) && regEx2.test(end.val())) {
+        $('#phCheckimg').attr('src', '');
+        $('#phCheckimg').attr('src', 'img/파란체크.png');
+        $('#divisionLine').css('color', 'black');
+        phMessage.text('');
     } else {
-        message.text('');
-        message.css('color', '#dc3340');
-        message.text('전화번호를 정확히 입력 해 주세요');
+        phMessage.text('');
+        phMessage.css('color', '#dc3340');
+        phMessage.text('전화번호를 정확히 dd입력 해 주세요');
+        $('#phCheckimg').attr('src', 'img/검정체크.png');
     }
 });
 
-//이메일
+//*이메일------------------------------------------------
+let email = $('#inputEmail');
+let adress = $('#inputAdress');
+let emailMessage = $("span[name ='emailErrorMessage']");
 
-//가입버튼
-let genderMessage = $("span[name = 'genderErrorMessage']");
-function validate() {
-    if (!gender[0].checked && !gender[1].checked) {
-        genderMessage.test('필수 정보입니다.');
-        $('#genderCheck').css('color', '#dc3340');
-        return false;
+function selectAdress() {
+    const select = $('#adress option:selected').val();
+    console.log($('#atMark').css('color'));
+    adress.val('');
+    adress.val(select);
+
+    if (adress.val().length > 0) {
+        $('#atMark').css('color', '');
+        $('#atMark').css('color', 'black');
     }
 }
+
+$('.email').on('propertychange change keyup paste input', function () {
+    const regEx1 = /^[a-z0-9_\.-]{2,10}$/;
+    const regEx2 = /^[a-z.]{2,10}$/;
+
+    if (regEx1.test(email.val()) && regEx2.test(adress.val())) {
+        emailMessage.text('');
+    } else {
+        emailMessage.css('color', '#dc3340');
+        emailMessage.text('이메일 주소를dd 다시 확인 해 주세요');
+    }
+});
+
+$('.email').on('click', function () {
+    $('#atMark').css('color', '');
+    $('#atMark').css('color', 'black');
+});
+$('.email').blur(function () {
+    if (email.val().length == 0 && adress.val().length == 0) {
+        $('#atMark').css('color', '');
+        $('#atMark').css('color', 'white');
+    }
+});
+
+$('#inputEmail').blur(function () {
+    if (email.val().length > 0) {
+        $('#atMark').css('color', '');
+        $('#atMark').css('color', 'black');
+        emailMessage.text('');
+    } else if ((email.val().length = 0)) {
+        $('#atMark').css('color', '');
+        $('#atMark').css('color', 'white');
+        emailMessage.css('color', '#dc3340');
+        emailMessage.text('이메일 주소를 다시 확인 해 주세요');
+    }
+});
+
+$('#inputAdress').blur(function () {
+    console.log($('#atMark').css('color'));
+    console.log(adress.val().length);
+    if (adress.val().length > 0) {
+        $('#atMark').css('color', '');
+        $('#atMark').css('color', 'black');
+        emailMessage.text('');
+    } else if (adress.val().length == 0) {
+        $('#atMark').css('color', '');
+        $('#atMark').css('color', 'white');
+        emailMessage.css('color', '#dc3340');
+        emailMessage.text('이메일 주소를 다시 확인 해 주세요');
+    }
+});
+//*가입버튼------------------------------------------------
+
+// function validate() {
+//     if()
+// }
